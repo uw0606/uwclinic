3a09b427544757498023961c8de46181a4934a17
uw0606 <103592210+uw0606@users.noreply.github.com>
2022-06-01 21:41:41 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -34,9 +34,9 @@
     instrument.addEventListener('click', () => {
       window.location.href = "https://uw0606.github.io/uwtools/"
    });
-  //   instrument.addEventListener('click', () => {
-  //     window.location.href = "https://uw0606.github.io/uwtools/"
-  //  });
+    instrument.addEventListener('click', () => {
+      window.location.href = "https://uw0606.github.io/uwmp/"
+   });
 }
 
 // サイトメニューアニメーション
@@ -61,4 +61,30 @@
   document.querySelectorAll('.animate').forEach(el => {
     observer.observe(el);
   });
+
+
+  // const sitemenu = document.querySelector('.animate');
+  // sitemenu.addEventListener('mouseover', () => {
+  //   sitemenu.style.background = 'rgb(231, 231, 231, .5)';
+  // }, false);
+  // sitemenu.addEventListener('mouseleave', () => {
+  //   sitemenu.style.background = 'rgb(231, 231, 231)';
+  // }, false);
+
+
+  // const sitemenu1 = document.querySelector('.animate.delay1');
+  // sitemenu1.addEventListener('mouseover', () => {
+  //   sitemenu1.style.background = 'rgb(231, 231, 231, .5)';
+  // }, false);
+  // sitemenu1.addEventListener('mouseleave', () => {
+  //   sitemenu1.style.background = 'rgb(231, 231, 231)';
+  // }, false);
+
+  // const sitemenu2 = document.querySelector('.animate.delay');
+  // sitemenu2.addEventListener('mouseover', () => {
+  //   sitemenu2.style.background = 'rgb(231, 231, 231, .5)';
+  // }, false);
+  // sitemenu2.addEventListener('mouseleave', () => {
+  //   sitemenu2.style.background = 'rgb(231, 231, 231)';
+  // }, false);
 }
